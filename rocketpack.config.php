8756af86ebca18032ad19f8c7a4337f5adf9db83
Iain Dooley <iain@workingsoftware.com.au>
2011-09-24 14:52:27 +1000

    Added dependency for Args package

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,9 +1,9 @@
 <?php
     rocketpack\Install::package('Murphy',array(0,0,0));
 
-    $dependencies[] = function()
+    Dependencies::register(function()
     {
         rocketpack\Dependency::forPackage('Murphy')
         ->add('Args',array(0,1,0))
         ->verify();
-    };
+    });
