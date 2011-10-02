41ac787d52118f25ad1495b2d83a0a4398933d4b
Iain Dooley <iain@workingsoftware.com.au>
2011-10-02 12:21:13 +1100

    Removed vestigial class

--- test_config.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class TestConfig implements rocketsled\Runnable
-    {
-        public function run()
-        {
-            echo 'TEST_USER: '.murphy\TEST_USER.PHP_EOL;
-            echo 'TEST_PASS: '.murphy\TEST_PASS.PHP_EOL;
-        }
-    }
