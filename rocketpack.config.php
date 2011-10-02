d0551055bbf699fe97ec0c8c3b585beb1ec34d86
Iain Dooley <iain@workingsoftware.com.au>
2011-10-02 12:27:22 +1100

    Added new namespaced Dependencies call

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,7 +1,7 @@
 <?php
     rocketpack\Install::package('Murphy',array(0,0,0));
 
-    Dependencies::register(function()
+    rocketpack\Dependencies::register(function()
     {
         rocketpack\Dependency::forPackage('Murphy')
         ->add('Args',array(0,1,0))
