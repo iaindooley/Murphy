575a4651b8319f5a018ce82bb1ed6dfdc88c7f5d
Iain Dooley <iain@workingsoftware.com.au>
2012-07-04 15:12:52 +1000

    Final namespace fixes and bumping version number and that of dependencies

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,9 +1,9 @@
 <?php
-    RocketPack\Install::package('https://github.com/iaindooley/Murphy',array(0,1,0));
+    RocketPack\Install::package('https://github.com/iaindooley/Murphy',array(0,2,0));
 
     RocketPack\Dependencies::register(function()
     {
         RocketPack\Dependency::forPackage('https://github.com/iaindooley/Murphy')
-        ->add('https://github.com/iaindooley/Args',array(0,2,0))
+        ->add('https://github.com/iaindooley/Args',array(0,2,1))
         ->verify();
     });
