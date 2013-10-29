d6a8ab7ebe44c89599d1e1ceb78f3f0d02ff9a87
Iain Dooley <iain@workingsoftware.com.au>
2013-10-30 08:02:00 +1100

    Removed commit hash from Args dependency

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -2,6 +2,6 @@
     RocketPack\Dependency::register(function()
     {
         RocketPack\Dependency::into(dirname(__FILE__).'/../')
-        ->add('https://github.com/iaindooley/Args','b5ad86d1193eb7efbe7be3ba26ff5b4e5a0476d4')
+        ->add('https://github.com/iaindooley/Args')
         ;
     });
