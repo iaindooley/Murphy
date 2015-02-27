9c2370f5d6ffa16d053b7bddb2ae76e693ded083
Chaker Ben Hammouda <benhammouda.chaker@gmail.com>
2015-02-27 20:47:36 +0100

    Added  paremeter to both load() and also() functions

--- fixture.class.php
+++ fixture.class.php
@@ -1,32 +1,32 @@
 <?php
     namespace Murphy;
     use Exception,Closure,Args;
-    
+
     class Fixture
     {
         private $callbacks;
         private $data;
         private static $instance = NULL;
-        
+
         private function __construct()
         {
             $this->callbacks = array();
             $this->data = array();
         }
-        
+
         private static function instance()
         {
             if(self::$instance === NULL)
                 self::$instance = new Fixture();
-            
+
             return self::$instance;
         }
-        
+
         public static function add(Closure $callback)
         {
             self::instance()->callbacks[] = $callback;
         }
-        
+
         public function execute(Closure $db_connect = NULL)
         {
             $databases = array();
@@ -37,14 +37,14 @@
                 {
                     if(!isset($d['database']))
                         throw new InvalidFixtureFormatException('You have included a @tables directive for '.$d['tables'].' but no @database directive');
-                    
+
                     if(!isset($databases[$d['database']]))
                         $databases[$d['database']] = array();
-                        
+
                     $databases[$d['database']] = array_merge($databases[$d['database']],$d['tables']);
                 }
             }
-            
+
             $aliases  = array();
 
             if(count($databases))
@@ -54,14 +54,14 @@
                     echo 'You need to include mysql_root in the command line arguments'.PHP_EOL;
                     exit(1);
                 }
-                    
+
                 foreach($databases as $database => $tables)
                 {
                     mysql_connect('localhost','root',$mysql_root);
                     mysql_select_db($database);
                     $tables = array_unique($tables);
                     $create_table_statements = array();
-                    
+
                     foreach($tables as $table)
                     {
                         if(!$query = mysql_query('SHOW CREATE TABLE `'.$table.'`'))
@@ -80,11 +80,11 @@
                     mysql_query('DROP DATABASE IF EXISTS `'.$alias.'`') or die(mysql_error());
                     mysql_query('CREATE DATABASE `'.$alias.'`') or die(mysql_error());
                     mysql_select_db($alias);
-                    
+
                     foreach($create_table_statements as $stmt)
                         mysql_query($stmt) or die(mysql_error());
                 }
-                
+
                 if(!$db_connect instanceof Closure)
                     throw new DbFixtureConnectionException('You have included database fixtures without a callback to pass connection details to');
             }
@@ -95,7 +95,7 @@
                     mysql_select_db($aliases[$d['database']][3]);
 
                 $args = array();
-    
+
                 foreach($d['rows'] as $row)
                 {
                     foreach($row as $index => $line)
@@ -111,59 +111,59 @@
             self::$instance = NULL;
         }
 
-        public function also($file)
+        public function also($database, $file)
         {
-            $this->extractFixtureDataFromFile($file);
+            $this->extractFixtureDataFromFile($database, $file);
             return $this;
         }
-        
-        public static function load($file)
+
+        public static function load($database, $file)
         {
-            self::instance()->extractFixtureDataFromFile($file);
+            self::instance()->extractFixtureDataFromFile($database, $file);
             return self::instance();
         }
-        
-        private function extractFixtureDataFromFile($path)
+
+        private function extractFixtureDataFromFile($database, $path)
         {
             require($path);
             $contents = file($path,FILE_IGNORE_NEW_LINES);
             $docblocks = array();
             $cur_docblock = NULL;
             $previous_docblock = NULL;
-        
+
             foreach($contents as $cont)
             {
                 if(strpos($cont,'/**') !== FALSE)
                     $cur_docblock = array();
-        
+
                 if(strpos($cont,'*/') !== FALSE)
                 {
                     $previous_docblock  = $cur_docblock;
                     $cur_docblock       = NULL;
                 }
-                
+
                 if($cur_docblock !== NULL)
                     $cur_docblock[] = $cont;
                 else if(preg_match('/Murphy\\\\Fixture::add\(/U',$cont,$matches))
                     $docblocks[] = $previous_docblock;
             }
-            
+
             $offset = count($this->data);
-        
+
             foreach($docblocks as $key => $block)
             {
                 $cur_index = $key+$offset;
                 $this->data[$key+$offset] = array('rows' => array());
-                $database = 'non_db_fixture_'.microtime(true).rand(0,9999);
 
                 foreach($block as $b)
                 {
                     if(strpos(trim($b),'/*') !== 0)
                     {
                         $b = trim(str_replace('*','',$b));
-                        
-                        if(strpos($b,'@database ') === 0)
-                            $database = trim(str_replace('@database','',$b));
+
+                        if(strpos($b,'@database ') === 0){
+                            //$database = trim(str_replace('@database','',$b));
+                        }
                         else if(strpos($b,'@tables ') === 0)
                             $this->data[$cur_index]['tables'] = explode(',',trim(str_replace('@tables','',$b)));
                         else if(!isset($this->data[$cur_index]['header']))
@@ -172,10 +172,10 @@
                             $this->data[$cur_index]['rows'][] = array_map('trim',explode('|',$b));
                     }
                 }
-                
+
                 if(!$database)
                     throw new InvalidFixtureFormatException('You must specify the @database directive for fixture: '.$fixture_name.' in: '.$path);
-                    
+
                 $this->data[$cur_index]['database'] = $database;
             }
         }
