269ffba71e6271966d880eb6ace6885219512cde
Iain Dooley <iain@workingsoftware.com.au>
2015-10-12 21:28:42 +1100

    Added try/catch block around the call to the test function so that if someone adds a catchall try/catch block in index.php we still get exceptions in the error output

--- test.class.php
+++ test.class.php
@@ -37,9 +37,59 @@
             require($path);
             
             foreach(self::instance()->tests as $test)
-                $test($this);
+            {
+                try
+                {
+                    $test($this);
+                }
+                
+                catch(Exception $exc)
+                {
+                    echo PHP_EOL.self::getExceptionTraceAsString($exc).PHP_EOL;
+                }
+            }
             
             exit(0);
+        }
+
+        /**
+        * copy/paste from http://stackoverflow.com/questions/1949345/how-can-i-get-the-full-string-of-php-s-gettraceasstring
+        */
+        public static function getExceptionTraceAsString($exception) {
+            $rtn = "";
+            $count = 0;
+            foreach ($exception->getTrace() as $frame) {
+                $args = "";
+                if (isset($frame['args'])) {
+                    $args = array();
+                    foreach ($frame['args'] as $arg) {
+                        if (is_string($arg)) {
+                            $args[] = "'" . $arg . "'";
+                        } elseif (is_array($arg)) {
+                            $args[] = "Array";
+                        } elseif (is_null($arg)) {
+                            $args[] = 'NULL';
+                        } elseif (is_bool($arg)) {
+                            $args[] = ($arg) ? "true" : "false";
+                        } elseif (is_object($arg)) {
+                            $args[] = get_class($arg);
+                        } elseif (is_resource($arg)) {
+                            $args[] = get_resource_type($arg);
+                        } else {
+                            $args[] = $arg;
+                        }   
+                    }   
+                    $args = join(", ", $args);
+                }
+                $rtn .= sprintf( "#%s %s(%s): %s(%s)\n",
+                                         $count,
+                                         $frame['file'],
+                                         $frame['line'],
+                                         $frame['function'],
+                                         $args );
+                $count++;
+            }
+            return $rtn;
         }
         
         public function pass()
